39ddb43311fd5398a194ecb8ca20acf6ef3b34f0
Thilini Dharmawardhana <dhanu.thilini@gmail.com>
2024-02-20 21:02:40 -0600

    Python flask routes

---
+++ petfax/__init__.py
@@ -0,0 +1,26 @@
+
+from flask import Flask 
+
+def create_app(): 
+    app = Flask(__name__)
+
+    @app.route('/')
+    def hello(): 
+        return 'Hello, PetFax!'
+
+# # pets index route
+#     @app.route('/pets')
+#     def pets(): 
+#         return 'These are our pets available for adoption!'
+# factory
+
+
+    # register pet blueprint 
+    from . import pet
+    app.register_blueprint(pet.bp)
+
+    # return the app 
+    return app
+
+
+
