fe08dbdaa060993679e49b86145215700539f2a8
Thilini Dharmawardhana <dhanu.thilini@gmail.com>
2024-02-25 14:04:48 -0600

    updated code-Day6

--- petfax/fact.py
+++ petfax/fact.py
@@ -12,6 +12,8 @@ def index():
     if request.method == 'POST':
        print(request.form)
        return redirect('/facts')
-    
-    return render_template('facts/fact_submit.html')
 
+
+@bp.route('/new')
+def new(): 
+    return render_template('facts/new.html')
